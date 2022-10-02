a521d7b13e05aee670993c33a1d4fd166e949e7b
Ionut Trif <trifionut207@gmail.com>
2022-10-02 20:26:59 +0300

    Created the functions for video controller

--- server/controllers/videoController.js
+++ server/controllers/videoController.js
@@ -108,16 +108,23 @@ export const sub = async (req, res, next) => {
 };
   
 export const getByTag = async (req, res, next) => {
+    const tags = req.query.tags.split(",");
     try {
-
+        const videos = await Video.find({tags:{$in: tags} }).limit(20);
+        res.status(200).json(videos);
     } catch (err) {
       next(err);
     }
   };
   
 export const search = async (req, res, next) => {
-  try {
-  } catch (err) {
-    next(err);
-  }
+    const query = req.query.q;
+    try {
+      const videos = await Video.find({
+        title: { $regex: query, $options: "i" },
+      }).limit(40);
+      res.status(200).json(videos);
+    } catch (err) {
+      next(err);
+    }
 };
