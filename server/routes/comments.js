4d4d914b5bb567f172538885e25be6aa98ea1052
Ionut Trif <trifionut207@gmail.com>
2022-10-02 21:02:39 +0300

    Small fix

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addComment, deleteComment, getComments } from "../controllers/commentController";
+import { addComment, deleteComment, getComments } from "../controllers/commentController.js";
 import {verifyToken} from "../verifyToken.js"
 const router = express.Router();
 
