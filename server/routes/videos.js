12c14a893d24088d1e253cfe0ab5e6260bbca988
Ionut Trif <trifionut207@gmail.com>
2022-10-02 21:09:59 +0300

    Small fix

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,19 +1,19 @@
 import express from "express";
-import { addVideo, updateVideo } from "../controllers/videoController.js";
+import { addVideo, updateVideo, getVideo, trend, addView, random, sub, getByTag, search } from "../controllers/videoController.js";
 import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
-//create a video
+//routes
 router.post("/", verifyToken, addVideo)
 router.put("/:id", verifyToken, updateVideo)
-// router.delete("/:id", verifyToken, addVideo)
-// router.get("/find/:id", getVideo)
-// router.put("/view/:id", addView)
-// router.get("/trend", trend)
-// router.get("/random", random)
-// router.get("/sub",verifyToken, sub)
-// router.get("/tags", getByTag)
-// router.get("/search", search)
+router.delete("/:id", verifyToken, addVideo)
+router.get("/find/:id", getVideo)
+router.put("/view/:id", addView)
+router.get("/trend", trend)
+router.get("/random", random)
+router.get("/sub",verifyToken, sub)
+router.get("/tags", getByTag)
+router.get("/search", search)
 
 export default router;
